43bd8ac3287cbb294b304a235dc06c515ed05b3e
Tevin Mantock <tmantock@gmail.com>
2016-09-01 23:04:50 -0700

    initializing repo

---
+++ README.md
@@ -0,0 +1,3 @@
+#Swift
+
+Learning Apple's new programming language
---
+++ intro.swift
@@ -0,0 +1,34 @@
+//variables
+var greeting: String = "Hello World"
+greeting = "Hello Swift"
+
+//immutables
+let newGreeting: String = "Hello World"
+newGreeting = "Hello Swift" //Error will occur during compile time
+
+//Data types
+var language: String = "Swift"
+var version: Double = 3.0
+var latestRelease: Int = 2016
+var isAwesome: Bool = true
+
+let title = "Welcome to Swift" // Swift can determine the datat type without the need for it be stated
+let statement = "trying things out"
+let concatenation = title + " - " + statement
+
+let today = "September 1, 2016"
+let metaData = "Today's date is \(today)" //String interpolation
+
+var array: [String] = ["Jack","Titanic","Headboard"] //Arrays can only store same data types
+
+println(array[0]) //Print to the screen on a new line
+print(array[1]) //print to screen withouth any escape characters
+
+//mutate the array
+array += "Icy water"
+array += ["Sadness","Cold"]
+array[0] = "Jack?"
+array[2..<5] = ["Enough with the sadness"]
+
+//disctionary (objects in swift)
+var user = ["name" : "Tevin", age : 22]
